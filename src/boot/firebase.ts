fb5775df1e729b2865c9f20882e7a8c72c2c6e41
Yazeed Alfuqaha <yalfuqaha8@gmail.com>
2026-03-25 12:16:58 +0300

    Security: Move Firebase config to environment variables and update gitignore

--- src/boot/firebase.ts
+++ src/boot/firebase.ts
@@ -2,20 +2,17 @@ import { boot } from 'quasar/wrappers'
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyD1GZgmopPI2yj9ep9DqI37woIXCvh4BkM",
-  authDomain: "lernova-1db47.firebaseapp.com",
-  projectId: "lernova-1db47",
-  storageBucket: "lernova-1db47.firebasestorage.app",
-  messagingSenderId: "407093213467",
-  appId: "1:407093213467:web:22587e4177d62d554bbbbe"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 
 export { auth }
-
 export default boot(() => {})
